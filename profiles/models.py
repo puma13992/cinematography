b2b2e66e5558cc2dc73c403ee5de02a72559e8c5
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-10-20 13:11:07 +0000

    Create profiles model and register to admin.py

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django_resized import ResizedImageField
 
-# Create your models here.
+
+# Code from CI walkthrough Django Rest Framework; slightly modified
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    image = ResizedImageField(
+        size=[300, 300],
+        upload_to='cinematography/',
+        default='../cinematography/ucqo5sjbdjholxwenlds',
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
