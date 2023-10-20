359ddfcc11d86bfc4c7c2b0f54594b4cab18c94a
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-10-20 13:55:11 +0000

    Add permissions.py and api-auth url path for DRF

---
+++ backend_cinematography/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+
+# Code from CI walkthrough Django Rest Framework
+class IsOwnerOrReadOnly(permissions.BasePermission):
+  def has_object_permission(self, request, view, obj):
+    if request.method in permissions.SAFE_METHODS:
+      return True
+    return obj.owner == request.user
