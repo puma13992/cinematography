0a73a7b2e559ea6e847384bdeed272a6132964d7
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-10-23 11:01:21 +0000

    Add model for glossary

--- glossary/models.py
+++ glossary/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Glossary(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_glossary_term')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='updated_glossary_term')
+    title = models.CharField(max_length=255, blank=False)
+    content = models.TextField(blank=False)
+
+    class Meta:
+        ordering = ['-updated_at']
+
+    def __str__(self):
+        return f'{self.title}'
