b817935f51ec507dccb13a77ebd3a5cd9c7f9af4
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-10-23 06:24:27 +0000

    Add movie list view and urls

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Movie
+from .serializers import MovieSerializer
 
-# Create your views here.
+
+# Code from CI walkthrough Django Rest Framework; slightly modified
+class MovieList(APIView):
+    serializer_class = MovieSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(
+            movies, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MovieSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
