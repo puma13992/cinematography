7799fb3ae3906be90e915d71f9b39f3d7067f3e5
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-10-23 11:05:42 +0000

    Add views and specific permission for glossary

--- backend_cinematography/permissions.py
+++ backend_cinematography/permissions.py
@@ -7,3 +7,11 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     if request.method in permissions.SAFE_METHODS:
       return True
     return obj.owner == request.user
+
+
+# Allow only created_by users to delete their glossary items
+class IsOwnerGlossary(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'DELETE':
+            return obj.created_by == request.user
+        return False
--- glossary/views.py
+++ glossary/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from .models import Glossary
+from .serializers import GlossarySerializer
+from backend_cinematography.permissions import IsOwnerGlossary
 
-# Create your views here.
+
+class GlossaryList(generics.ListCreateAPIView):
+    queryset = Glossary.objects.all()
+    serializer_class = GlossarySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user, updated_by=self.request.user)
+
+
+class GlossaryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Glossary.objects.all()
+    serializer_class = GlossarySerializer
+
+    def perform_update(self, serializer):
+        # Set the updated_by field to the current authenticated user
+        serializer.validated_data['updated_by'] = self.request.user
+        serializer.save()
+
+    def get_permissions(self):
+        if self.request.method == 'PUT':
+            # Only users with IsAuthenticated authorisation have access to the edit form
+            return [IsAuthenticated()]
+
+        elif self.request.method == 'DELETE':
+            # Only users with IsOwner authorisation can delete
+            return [IsOwnerGlossary()]
+        
+        else:
+            return [IsAuthenticatedOrReadOnly()]
