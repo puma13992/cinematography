4f051e98b31eb4a28f7b712c3b32ce4d2d7f025f
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-10-23 13:42:46 +0000

    Add extra fields for movies count in profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,6 +6,7 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    movies_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -15,5 +16,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner'
+            'content', 'image', 'is_owner', 'movies_count',
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,22 +1,37 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from backend_cinematography.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
+# Code from CI walkthrough Django Rest Framework; slightly modified
 class ProfileList(generics.ListAPIView):
     """
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        movies_count=Count('owner__movie', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    ordering_fields = [
+        'movies_count',
+    ]
 
 
+# Code from CI walkthrough Django Rest Framework; slightly modified
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        movies_count=Count('owner__movie', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
