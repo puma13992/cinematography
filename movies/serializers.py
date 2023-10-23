f6f415370728bc70af9b5b8d6bc92a85a86c9db1
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-10-23 06:11:52 +0000

    Add serializer for movies

---
+++ movies/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from taggit.serializers import (TagListSerializerField,
+                                TaggitSerializer)
+from wishlist.models import Wishlist
+from .models import Movie
+
+
+# Code basic from CI walkthrough Django Rest Framework; modified
+class MovieSerializer(TaggitSerializer, serializers.ModelSerializer):
+    categories = TagListSerializerField()
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Movie
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'release', 'director', 'content',
+            'image', 'categories',
+            ]
+
