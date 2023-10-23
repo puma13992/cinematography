eeb1a868d8697d887156e4bca9c118c48e29d9a0
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-10-23 13:40:14 +0000

    Add extra fields for wishlist to movies serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from taggit.serializers import (TagListSerializerField,
                                 TaggitSerializer)
+from wishlists.models import Wishlist
 from .models import Movie
 
 
@@ -12,6 +13,7 @@ class MovieSerializer(TaggitSerializer, serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    wishlist_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -29,13 +31,21 @@ class MovieSerializer(TaggitSerializer, serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-
+    
+    def get_wishlist_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            wishlist = Wishlist.objects.filter(
+                owner=user, movie=obj
+            ).first()
+            return wishlist.id if wishlist else None
+        return None
+    
     class Meta:
         model = Movie
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'release', 'director', 'content',
-            'image', 'categories',
+            'image', 'categories', 'wishlist_id',
             ]
-
