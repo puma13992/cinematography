00b5b3ac47cc9462f2ec55a229f5f62314d0a81e
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-10-23 06:35:33 +0000

    Register movies in admin.py

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'release', 'director', 'get_categories']
+
+    def get_categories(self, obj):
+        return u", ".join(o.name for o in obj.categories.all())
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('categories')
