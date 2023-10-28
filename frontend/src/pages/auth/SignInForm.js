15e91b2a5ffb468b47db43a118ffe6b4714fb144
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-10-28 20:27:36 +0000

    Rename styles module for auth forms

--- frontend/src/pages/auth/SignInForm.js
+++ frontend/src/pages/auth/SignInForm.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { Form, Button, Col, Row, Container, Alert } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 
-import styles from "../../styles/SignInUpForm.module.css";
+import styles from "../../styles/AuthForms.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
