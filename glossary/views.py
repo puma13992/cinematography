779d7c63b0ebc5f7cc82f52e830a288d7c600f5b
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-11-03 09:20:51 +0000

    Extend views for glossary

--- glossary/views.py
+++ glossary/views.py
@@ -1,14 +1,26 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from backend_cinematography.permissions import IsOwnerGlossary
 from .models import Glossary
 from .serializers import GlossarySerializer
-from backend_cinematography.permissions import IsOwnerGlossary
 
 
 class GlossaryList(generics.ListCreateAPIView):
     queryset = Glossary.objects.all()
     serializer_class = GlossarySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    search_fields = [
+        'title',
+    ]
+    ordering_fields = [
+        'title',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user, updated_by=self.request.user)
@@ -26,11 +38,11 @@ class GlossaryDetail(generics.RetrieveUpdateDestroyAPIView):
     def get_permissions(self):
         if self.request.method == 'PUT':
             # Only users with IsAuthenticated authorisation have access to the edit form
-            return [IsAuthenticated()]
+            return [IsAuthenticated()]        
 
         elif self.request.method == 'DELETE':
             # Only users with IsOwner authorisation can delete
             return [IsOwnerGlossary()]
-     
+      
         else:
             return [IsAuthenticatedOrReadOnly()]
