a2019369827aea4e690621fa61ecafe0f739d1a4
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-11-03 09:24:19 +0000

    Add use redirect hook

---
+++ frontend/src/hooks/useRedirect.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import useAlert from "./useAlert";
+
+// Code from https://github.com/artcuddy/project5-foodsnap-frontend/blob/main/src/hooks/useRedirect.js; slightly modified
+export const useRedirect = (userAuthStatus) => {
+	const history = useHistory();
+	const { setAlert } = useAlert();
+
+	useEffect(() => {
+		const handleMount = async () => {
+			try {
+				await axios.post("/dj-rest-auth/token/refresh/");
+				// if user is logged in, the code below will run
+				if (userAuthStatus === "loggedIn") {
+					history.push("/");
+				}
+			} catch (err) {
+				// if user is not logged in, the code below will run
+				if (userAuthStatus === "loggedOut") {
+					setAlert(
+						"This action is only allowed for logged in users.",
+						"warning"
+					);
+					history.push("/");
+				}
+			}
+		};
+
+		handleMount();
+	}, [history, userAuthStatus, setAlert]);
+};
