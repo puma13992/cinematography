a6c1032d53b36c53a76bbc6c837227968dd6347e
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-11-08 07:29:40 +0000

    Add test for navbar

---
+++ frontend/src/components/__tests__/NavBar.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from "../NavBar";
+import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
+
+test("renders NavBar Home Link", () => {
+	render(
+		<Router>
+			<NavBar />
+		</Router>
+	);
+	// screen.debug();
+	const homeLink = screen.getByRole("link", { name: "Home" });
+	expect(homeLink).toBeInTheDocument();
+});
+
+test("renders link to the wishlist page for a logged in user", async () => {
+	render(
+		<Router>
+			<CurrentUserProvider>
+				<NavBar />
+			</CurrentUserProvider>
+		</Router>
+	);
+
+	const wishlist = await screen.findByText("Wishlist");
+	expect(wishlist).toBeInTheDocument();
+});
