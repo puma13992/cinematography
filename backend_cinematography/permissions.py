0c5eed02d7381a4bea928e209ab4f865416be541
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-11-09 10:29:20 +0000

    Add some docstrings & PEP8 validation to files in api/backend folder

--- backend_cinematography/permissions.py
+++ backend_cinematography/permissions.py
@@ -3,14 +3,23 @@ from rest_framework import permissions
 
 # Code from CI walkthrough Django Rest Framework
 class IsOwnerOrReadOnly(permissions.BasePermission):
-  def has_object_permission(self, request, view, obj):
-    if request.method in permissions.SAFE_METHODS:
-      return True
-    return obj.owner == request.user
+    """
+    Permission to read only or make
+    requests for owner only
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
 
 
-# Allow only created_by users to delete their glossary items
 class IsOwnerGlossary(permissions.BasePermission):
+    """
+    Allow only created_by users
+    to delete their glossary items
+    """
+
     def has_object_permission(self, request, view, obj):
         if request.method == 'DELETE':
             return obj.created_by == request.user
--- backend_cinematography/urls.py
+++ backend_cinematography/urls.py
@@ -15,22 +15,33 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route, logout_route
 from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
+from .views import root_route, logout_route
+
 
 urlpatterns = [
-    path('', root_route),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path("", root_route),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
     # our logout route has to be above the default one to be matched first
-    path('dj-rest-auth/logout/', logout_route),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('', include('profiles.urls')),
-    path('', include('movies.urls')),
-    path('', include('comments.urls')),
-    path('', include('wishlists.urls')),
-    path('', include('glossary.urls')),
-    path('dj-rest-auth/password/reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('dj-rest-auth/password/reset/confirm/<uid>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path("dj-rest-auth/logout/", logout_route),
+    path("dj-rest-auth/", include("dj_rest_auth.urls")),
+    path(
+        "dj-rest-auth/registration/", include(
+            "dj_rest_auth.registration.urls")),
+    path("", include("profiles.urls")),
+    path("", include("movies.urls")),
+    path("", include("comments.urls")),
+    path("", include("wishlists.urls")),
+    path("", include("glossary.urls")),
+    path(
+        "dj-rest-auth/password/reset/",
+        PasswordResetView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "dj-rest-auth/password/reset/confirm/<uid>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
 ]
