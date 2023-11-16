d1ee67d59fe3392509f3c377f021a62cb88806e7
Sarah Topfstädt <st1.webmail@yahoo.de>
2023-11-16 17:09:54 +0000

    Add delete modal component to frontend

---
+++ frontend/src/components/DeleteModal.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+
+const DeleteMovieModal = ({ show, onCancel, onConfirm }) => {
+  return (
+    <Modal show={show} onHide={onCancel}>
+      <Modal.Header closeButton>
+        <Modal.Title>Confirm Deletion</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Are you sure you want to delete this?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onCancel}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={onConfirm}>
+          Delete
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default DeleteMovieModal;
